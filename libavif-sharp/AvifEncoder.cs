88e612a6a4b21dce3763c6b02b5945fd10f47eee
Yasuhiro Taniuchi <tan-y@aosoft.jp>
2021-04-06 01:08:06 +0900

    Add AvifEncoder

---
+++ libavif-sharp/AvifEncoder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using LibAvif.Binding;
+
+namespace LibAvif
+{
+    public sealed class AvifEncoder : IDisposable
+    {
+        private bool _disposedValue;
+        private IntPtr _native;
+
+        public AvifEncoder()
+        {
+            _native = libavif.avifEncoderCreate();
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                libavif.avifEncoderDestroy(_native);
+                _native = IntPtr.Zero;
+                _disposedValue = true;
+            }
+        }
+
+        ~AvifEncoder()
+        {
+            Dispose(disposing: false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
